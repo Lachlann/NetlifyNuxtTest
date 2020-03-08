30bd9b979d1f759233e11a8d1f62d37cce48f260
Lachlann Rattray <lachlann@screenmedia.co.uk>
2020-03-08 20:40:35 +0000

    added blog index
    - added blog index
    - Added tags to config

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,4 +1,16 @@
-<template></template>
+<template>
+<section>
+  <article v-for="blog in blogPosts" v-bind:key="blog.title" >
+    <h1>
+      <a :href="`/blog/${blog.slug}`">
+        {{blog.title}}
+      </a>
+    </h1>
+
+  </article>
+
+</section>
+</template>
 <script>
 export default {
   computed: {
